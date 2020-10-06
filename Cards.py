f41b118241371cab9835d153ea2fbcb850b2ea84
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2020-10-06 17:23:40 -0400

    Added ability to shuffle

---
+++ Cards.py
@@ -0,0 +1,17 @@
+import random
+
+cardlist = []
+for outer in range(1, 5):
+    for inner in range(1, 14):
+        cardlist.append(inner)
+
+
+
+random.shuffle(cardlist)
+
+#Shuffle
+print(cardlist)
+
+#Shuffle
+random.shuffle(cardlist)
+print(cardlist)
